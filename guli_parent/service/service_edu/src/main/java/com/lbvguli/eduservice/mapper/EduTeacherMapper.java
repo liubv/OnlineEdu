cb730fc440a79b5d5bd6c98b09cc9485a181c88c
liubv <liubv923@163.com>
2020-11-07 16:40:58 +0800

    auto code generator

---
+++ guli_parent/service/service_edu/src/main/java/com/lbvguli/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.lbvguli.eduservice.mapper;
+
+import com.lbvguli.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-11-07
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ guli_parent/service/service_edu/src/main/java/com/lbvguli/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lbvguli.eduservice.service.impl;
+
+import com.lbvguli.eduservice.entity.EduTeacher;
+import com.lbvguli.eduservice.mapper.EduTeacherMapper;
+import com.lbvguli.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-11-07
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
