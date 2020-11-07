6c925a006437cbcd6fdec1f08ff9e9d55130ebd9
liubv <liubv923@163.com>
2020-11-07 21:20:22 +0800

    unify return type

--- guli_parent/service/service_edu/src/main/java/com/lbvguli/eduservice/EduApplication.java
+++ guli_parent/service/service_edu/src/main/java/com/lbvguli/eduservice/EduApplication.java
@@ -2,8 +2,10 @@ package com.lbvguli.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.lbvguli"})
 public class EduApplication {
 
     public static void main(String[] args) {
--- guli_parent/service/service_edu/src/main/java/com/lbvguli/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/lbvguli/eduservice/controller/EduTeacherController.java
@@ -1,8 +1,12 @@
 package com.lbvguli.eduservice.controller;
 
 
+import com.lbvguli.commonutils.R;
 import com.lbvguli.eduservice.entity.EduTeacher;
 import com.lbvguli.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.List;
  * @author testjava
  * @since 2020-11-07
  */
+@Api(value = "讲师控制")
 @CrossOrigin
 @RestController
 @RequestMapping("/eduservice/teacher")
@@ -24,15 +29,24 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
 
+    @ApiOperation(value = "找到所有讲师")
     @GetMapping("findAll")
-    public List<EduTeacher> findAll(){
+    public R findAll(){
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
+    @ApiOperation(value = "按id删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeById(@PathVariable String id){
-        return teacherService.removeById(id);
+    public R removeById(@ApiParam(name = "id",value = "讲师id",required = true)
+                                  @PathVariable String id){
+
+        boolean flag = teacherService.removeById(id);
+        if(flag){
+            return R.ok();
+        }else{
+            return R.error();
+        }
     }
 }
 
